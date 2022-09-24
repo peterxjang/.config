9651770bb9c2a1dbb375161b377e3d3c1d719a2f
Peter Jang <peter.x.jang@gmail.com>
2022-09-24 10:57:19 -0500

    Replace autocomplete plugins with manual complete on <c-x><c-o>

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,10 +15,6 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
   Plug 'nvim-telescope/telescope.nvim'
   Plug 'neovim/nvim-lspconfig'
 	Plug 'jose-elias-alvarez/null-ls.nvim'
-	Plug 'hrsh7th/cmp-nvim-lsp'
-	Plug 'hrsh7th/nvim-cmp'
-	Plug 'hrsh7th/cmp-vsnip'
-	Plug 'hrsh7th/vim-vsnip'
 vim.call('plug#end')
 
 -- Shortcuts
@@ -64,46 +60,12 @@ vim.cmd [[imap <expr> <tab> emmet#expandAbbrIntelligent("\<tab>")]]
 -- treesitter
 require('nvim-treesitter.configs').setup({ highlight = { enable = true }})
 
--- nvim-cmp
-local cmp = require('cmp')
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert {
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-	},
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' }
-	})
-})
-
 -- lsp 
+vim.cmd [[autocmd CompleteDone * pclose]]
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local	on_attach = function(client, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
@@ -116,12 +78,9 @@ local	on_attach = function(client, bufnr)
 	end
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 require('lspconfig').elixirls.setup({
 	cmd = { "/usr/local/bin/elixir-ls/language_server.sh" },
-	on_attach = on_attach,
-	capabilities = capabilities,
+	on_attach = on_attach
 })
 
 require("null-ls").setup({
